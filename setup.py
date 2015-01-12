28cb1437430253029d97d07d065a7d84fd90b5d1
TimmWagener <wagenertimm@gmail.com>
2015-01-12 12:48:44 +0100

    1. Fixed distribution via PyPI. Added MANIFEST.in file to properly add data files to the package and be able to install them correctly via pip.

--- setup.py
+++ setup.py
@@ -14,20 +14,14 @@ from setuptools import find_packages
 
 # Setup
 # ------------------------------------------------------------------
-setup(name='renderthreads',
-      version='0.2.1',
-      description='Package that enables easy command-line multithreaded rendering for Nuke.',
-      url='https://github.com/timmwagener/renderthreads',
-      author='Timm Wagener',
-      author_email='wagenertimm@gmail.com',
-      license='MIT',
-      keywords='Foundy TheFoundry Nuke Pipeline Compositing Filmakademie Timm Wagener Multithreading Commandline Rendering',
-      packages=find_packages(),
-      package_data = {'renderthreads.media.ui': ['*'],
-      					'renderthreads.media.fonts': ['*'],
-      					'renderthreads.media.icons': ['*'],
-      					'renderthreads.lib.third_party.pysideuic': ['*'],
-      					'renderthreads.lib.third_party.pysideuic.Compiler': ['*'],
-      					'renderthreads.lib.third_party.pysideuic.port_v2': ['*'],
-      					'renderthreads.lib.third_party.pysideuic.widget-plugins': ['*']},
-      zip_safe=False)
+setup(name = 'renderthreads',
+      version = '0.2.2',
+      description = 'Package that enables easy command-line multithreaded rendering for Nuke.',
+      url = 'https://github.com/timmwagener/renderthreads',
+      author = 'Timm Wagener',
+      author_email = 'wagenertimm@gmail.com',
+      license = 'MIT',
+      keywords = 'Foundy TheFoundry Nuke Pipeline Compositing Filmakademie Timm Wagener Multithreading Commandline Rendering',
+      packages = find_packages(),
+      include_package_data = True,
+      zip_safe = False)
