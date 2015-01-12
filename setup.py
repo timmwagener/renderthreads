7a22a28893f85a90c356e6ffbdebc26c4c74870f
TimmWagener <wagenertimm@gmail.com>
2015-01-12 11:59:46 +0100

    1. Distribution via PyPI should work now.Fixed setup.py and added __init__.py files for setuptools find_packages(). Data files should now properly be added with setuptools sdist.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ from setuptools import find_packages
 # Setup
 # ------------------------------------------------------------------
 setup(name='renderthreads',
-      version='0.2',
+      version='0.2.1',
       description='Package that enables easy command-line multithreaded rendering for Nuke.',
       url='https://github.com/timmwagener/renderthreads',
       author='Timm Wagener',
@@ -23,4 +23,11 @@ setup(name='renderthreads',
       license='MIT',
       keywords='Foundy TheFoundry Nuke Pipeline Compositing Filmakademie Timm Wagener Multithreading Commandline Rendering',
       packages=find_packages(),
+      package_data = {'renderthreads.media.ui': ['*'],
+      					'renderthreads.media.fonts': ['*'],
+      					'renderthreads.media.icons': ['*'],
+      					'renderthreads.lib.third_party.pysideuic': ['*'],
+      					'renderthreads.lib.third_party.pysideuic.Compiler': ['*'],
+      					'renderthreads.lib.third_party.pysideuic.port_v2': ['*'],
+      					'renderthreads.lib.third_party.pysideuic.widget-plugins': ['*']},
       zip_safe=False)
